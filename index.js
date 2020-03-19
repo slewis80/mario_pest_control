d7530ef832d94d46ab760500e8f23fb8ce72d988
Sabrina <sabrina.lewis@bryanuniversity.edu>
2020-03-19 15:14:46 -0500

    linking html to javascript

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ form.addEventListener("submit", totalCost);
 function totalCost(e){
     e.preventDefault();
 
-    let goombas = form.goombas.value;
-    let bobombs = form.bobombs.value;
-    let cheepcheeps = form.cheepcheeps.value;
+    let numberOfGoombas = form.goombas.value;
+    let numberOfBobombs = form.bobombs.value;
+    let numberOfCheepcheeps = form.cheepcheeps.value;
 
-    const goombasCost = function countGoombas(goombas){
-        return (goombas * 5);
+    let goombasCost = function countGoombas(){
+        return (numberOfGoombas * 5);
     }
-    const bobombsCost = function countBobombs(bobombs){
-        return (bobombs * 7);
+    let bobombsCost = function countBobombs(){
+        return (numberOfBobombs * 7);
     }
-    const cheepcheepsCost = function countCheepcheeps(cheepcheeps){
-        return (cheepcheeps * 11);
+    let cheepcheepsCost = function countCheepcheeps(){
+        return (numberOfCheepcheeps * 11);
     }
     const result = goombasCost + bobombsCost + cheepcheepsCost;
-
-    document.getElementById("totalCost").innerHTML = `Your total cost is ${result} coins.`
-
 }
+document.getElementById("totalCost").innerHTML = `Your total cost is ${result} coins.`
