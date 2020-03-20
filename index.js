bcec75e3eb4a38bb5d1997f541c4eff7d1d8cd68
Sabrina <sabrina.lewis@bryanuniversity.edu>
2020-03-19 20:16:58 -0500

    working javascript form

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const form = document.form;
+const form = document.getElementById("monstersForm");
 
-form.addEventListener("submit", totalCost);
+function costOfMonster(num1, num2) {
+    return (num1 * num2);
+}
 
 function totalCost(e){
     e.preventDefault();
 
-    let numberOfGoombas = form.goombas.value;
-    let numberOfBobombs = form.bobombs.value;
-    let numberOfCheepcheeps = form.cheepcheeps.value;
+    let numberOfGoombas = costOfMonster(form.goombas.value, 5);
+    let numberOfBobombs = costOfMonster(form.bobombs.value, 7);
+    let numberOfCheepcheeps = costOfMonster(form.cheepcheeps.value, 11);
 
-    let goombasCost = function countGoombas(){
-        return (numberOfGoombas * 5);
-    }
-    let bobombsCost = function countBobombs(){
-        return (numberOfBobombs * 7);
-    }
-    let cheepcheepsCost = function countCheepcheeps(){
-        return (numberOfCheepcheeps * 11);
-    }
-    const result = goombasCost + bobombsCost + cheepcheepsCost;
+    let result = (numberOfGoombas + numberOfBobombs + numberOfCheepcheeps);
+
+    document.getElementById("totalCost").innerHTML = `Your total cost is ${result} coins.`;
+
 }
-document.getElementById("totalCost").innerHTML = `Your total cost is ${result} coins.`
+
+form.addEventListener("submit", totalCost);
